f75e2e54b29514c39e207dad641f6c6a6bea59b4
unknown <gloriazoldano@gmail.com>
2021-06-15 10:18:16 -0300

    dejé únicamente el mainRoutes.js

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,7 +2,13 @@ const express = require ('express');
 const router = express.Router();
 
 const mainController = require ('../controllers/mainController')
+const productController = require ('../controllers/productController')
+const usersController = require ('../controllers/usersController')
 
 router.get('/', mainController.index);
+router.get('/producto', productController.producto);
+router.get('/carrito', productController.carrito);
+router.get('/login', usersController.login);
+router.get('/registro', usersController.register);
 
 module.exports = router;
